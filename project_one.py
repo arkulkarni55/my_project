0d3d50053a305d0bb0051460b64bb803905610a1
Abhishek <114731541+arkulkarni55@users.noreply.github.com>
2023-12-07 20:49:53 +0530

    Update project_one.py

--- project_one.py
+++ project_one.py
@@ -5,7 +5,7 @@ import snowflake.connector
 import pandas as pd
 from sklearn import datasets
 from snowflake.snowpark import Session
-from snowflake.snowpark.functions import 
+from snowflake.snowpark.functions import *
 from sklearn.ensemble import RandomForestClassifier
 
 
